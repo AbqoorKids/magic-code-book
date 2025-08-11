d117d6a255896fec21f10f89f11a03dbeeb21bb6
AbqoorKids <abqoor.kids@gmail.com>
2025-08-11 20:25:45 +0400

    Create 06_friends_list.py

---
+++ lessons/06-for-loops-lists/06_friends_list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 06_friends_list.py
+# طباعة أسماء الأصدقاء
+
+my_friends = ["كركور", "فرفور", "قرقور"]
+
+# حلقة تمر على كل عنصر في القائمة
+for friend in my_friends:
+    # تطبع اسم كل صديق
+    print(friend)
