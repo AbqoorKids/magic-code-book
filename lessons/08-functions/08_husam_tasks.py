8f6a6b868d9235cac8ba99cb1f8acfc18a740bbe
AbqoorKids <abqoor.kids@gmail.com>
2025-08-11 20:57:38 +0400

    Create 08_husam_tasks.py

---
+++ lessons/08-functions/08_husam_tasks.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 08_husam_tasks.py
+# تابع باسم husam_tasks يحتوي على مهام حسام
+
+def husam_tasks():
+    print("ترتيب الأغراض")
+    print("رمي النفايات")
+
+# استدعاء التابع
+husam_tasks()
