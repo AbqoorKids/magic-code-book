3ef90c03e9d5ebd2a2cd73813f0750a72abcec47
AbqoorKids <abqoor.kids@gmail.com>
2025-08-11 19:45:18 +0400

    Create 04_types_demo.py

---
+++ lessons/04-smart-review-conditions/04_types_demo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 04_types_demo.py
+# مثال: التعرف على نوع البيانات
+
+print(type(False))   # نوع Boolean
+print(type("موز"))   # نوع String
+print(type(200))     # نوع Integer
