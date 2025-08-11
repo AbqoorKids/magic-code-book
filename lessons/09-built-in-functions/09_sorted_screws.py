7b2b1806a9d79d50def0621bc9120d47a0fff27c
AbqoorKids <abqoor.kids@gmail.com>
2025-08-11 21:06:18 +0400

    Create 09_sorted_screws.py

---
+++ lessons/09-built-in-functions/09_sorted_screws.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 09_sorted_screws.py
+# استخدام sorted() لترتيب قائمة أرقام
+
+# قائمة البراغي
+screws = [1, 7, 5, 4, 2, 3, 6, 8]
+
+# ترتيب البراغي
+sorted_screws = sorted(screws)
+
+print("البراغي بعد الترتيب:", sorted_screws)
